5c892618e437e8f80c3f0553d20a1ec2e2ae7e2d
Justin Munn <munnja001@gmail.com>
2014-10-08 00:21:37 -0500

    added support for uploading images to FTP, added some comments

---
+++ config.py
@@ -0,0 +1,8 @@
+# FTP config
+ftp_host="ftp.partyshipper.com"
+ftp_username="patiotoparlor@partyshipper.com"
+ftp_password="credera1!"
+
+# export config
+export_dir = 'magento-export'
+magento_filename = export_dir + '/magento.csv'
--- doba-to-magento.py
+++ doba-to-magento.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
-import csv, argparse, os, re
+import urllib2, csv, argparse, os, re, config
 
+parser = argparse.ArgumentParser(description='Process a Doba Product Export.')
+parser.add_argument('filename', type=str,
+                   help='the name of the file to parse')
+parser.add_argument('-c', '--category', nargs=1, default=[''])
 
+args = parser.parse_args()
+
+# Method to generate URL Slug (Copied from Internets)
 _slugify_strip_re = re.compile(r'[^\w\s-]')
 _slugify_hyphenate_re = re.compile(r'[-\s]+')
 def slugify(value):
@@ -18,11 +25,14 @@ def slugify(value):
     value = unicode(_slugify_strip_re.sub('', value).strip().lower())
     return _slugify_hyphenate_re.sub('-', value)
 
+# gets the filename from a URL
+def get_filename_from_url(url):
+    return url.split('/')[-1]
 
 magento_fieldnames = ["sku", "_attribute_set", "_type", "_category", "_product_websites", "url_key", "cost", "description", "image", "name", "price", "_media_image", "_media_attribute_id", "_media_position", "_media_is_disabled", "short_description", "small_image", "status", "tax_class_id", "thumbnail", "visibility", "weight", "qty", "backorders", "is_in_stock", "manage_stock"]
-
-def create_magento_dict(row, args):
-  image_url = '/' + row.get('image_file').split('/')[-1]
+    
+def create_magento_dict(row):
+  image_url = '/' + get_filename_from_url(row.get('image_file'))
   weight = '0.00' if not row.get('item_weight') else row.get('item_weight')
   return {
     "sku": row.get('product_sku'),
@@ -53,26 +63,32 @@ def create_magento_dict(row, args):
     "manage_stock": '1'
   }
 
-def create_inventory_dict(row):
-  return {
-    'sku': row.get('product_sku'),
-    'image_file': row.get('image_file'),
-    'small_image': row.get('image_file')
-  }
+# Ensure export dir exists
+if not os.path.exists(config.export_dir):
+    os.mkdir(config.export_dir)
 
-parser = argparse.ArgumentParser(description='Process a Doba Product Export.')
-parser.add_argument('filename', type=str,
-                   help='the name of the file to parse')
-parser.add_argument('-c', '--category', nargs=1, default=[''])
-
-args = parser.parse_args()
-export_dir = 'magento-export'
-if not os.path.exists(export_dir):
-    os.mkdir(export_dir)
-
-with open(args.filename, 'rbU') as inputfile, open(export_dir + '/magento.csv', 'w+') as magento_file:
+image_urls = []
+# Create Magento CSV from Doba CSV
+with open(args.filename, 'rbU') as inputfile, open(config.magento_filename, 'w+') as magento_file:
     product_reader = csv.DictReader(inputfile, delimiter=',', quotechar='"')
     magento_writer = csv.DictWriter(magento_file, delimiter=',', quotechar='"', fieldnames = magento_fieldnames, quoting=csv.QUOTE_ALL, lineterminator = '\n')
     magento_writer.writeheader()
     for row in product_reader:
-      magento_writer.writerow(create_magento_dict(row, args))
+      image_urls.append(row.get('image_file'))
+      magento_writer.writerow(create_magento_dict(row))
+    
+# Upload images via FTP  
+from ftplib import FTP
+if image_urls:
+    ftp = FTP(config.ftp_host, config.ftp_username, config.ftp_password)
+    ftp.cwd('/media/import')
+    ftp_files = ftp.nlst()
+    images_to_download = [x for x in image_urls if get_filename_from_url(x) not in ftp_files]
+    if image_urls: 
+        print "There are %s images to upload" % len(images_to_download)
+        for image_url in images_to_download:
+            print "Uploading: %s" % image_url
+            ftp.storbinary('STOR ' + get_filename_from_url(image_url), urllib2.urlopen(image_url))
+        print "Finished!"
+    else: print "No Images to Upload"
+else: print "No Images to Upload"
